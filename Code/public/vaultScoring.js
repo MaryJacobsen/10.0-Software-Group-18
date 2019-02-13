b9431c7b14a87eb4673c50d90a2e517cecdcef59
gilesni <jklihu@gmail.com>
2019-02-12 16:46:47 -0800

    Vaultscoring hopefully finished

--- Code/public/vaultScoring.js
+++ Code/public/vaultScoring.js
@@ -1,16 +1,6 @@
-var teams = [
-  ["OSU", "osu1", "osu2", "osu3", "osu4", "osu5", "osu6", "osu7", "osu8"],
-  ["UW", "uw1", "uw2", "uw3", "uw4", "uw5", "uw6", "uw7", "uw8"],
-  ["UO", "uo1", "uo2", "uo3", "uo4", "uo5", "uo6", "uo7", "uo8"],
-  ["UCLA", "ucla1", "ucla2", "ucla3", "ucla4", "ucla5", "ucla6", "ucla7", "ucla8"]
-];
-
-let once = {
-  once : true
-}
-
-var teamIndex;
-var playerIndex = 1;
+var players = [];
+var playerIndex = 0;
+var teamName;
 
 function getTeamData() {
   var url = window.location.origin;
@@ -29,8 +19,7 @@ window.onload = getTeamData();
 
 function getPlayers() {
   var url = window.location.origin;
-  console.log(this.value);
-  var players = [];
+  teamName = this.value;
   $.getJSON(url + "/player/" + this.value, (data) => {
     $.each(data, (key, val) => {
       players.push(val.name);
@@ -44,55 +33,43 @@ function getPlayers() {
 
 $("#team-select").one("change", getPlayers);
 
-var teamSelector = document.getElementById('team-select');
-teamSelector.addEventListener('change', getPlayers, once);
-
 function makeActive(event) {
-  var previousActive = document.getElementsByClassName('active');
-  if (previousActive[0]) {
-    previousActive[0].classList.remove('active');
-  }
+  $(".active").removeClass("active");
 
-  event.target.classList.add('active');
+  this.addClass("active");
 }
 
-var scoreButtons = document.getElementsByClassName('score-button');
-for (var i = 0; i < scoreButtons.length; i++) {
-  scoreButtons[i].addEventListener('click', makeActive);
-}
+$(".score-button").click(makeActive);
 
-function advancePlayers(event) {
-  console.log(teams[teamIndex]);
-  var active = document.getElementsByClassName('active');
-  if (active[0] == undefined) {
+function advancePlayers() {
+  if ($(".active") == undefined) {
     alert("Please select a score");
     return;
   }
 
-  var scoreObject = {
-    teamName: teams[teamIndex][0],
-    playerName: teams[teamIndex][playerIndex],
-    vaultScore: parseFloat(active[0].innerText)
+  scoreObject = {
+    vaultScore: parseFloat($(".active").text())
   }
 
-  console.log(scoreObject);
+  var url = window.location.origin;
+  $.ajax({
+    url: url + "player/" + $("#player-score-name").text(),
+    type: "put",
+    contentType: "application/JSON",
+    data: scoreObject,
+    success: () => {
+      playerIndex++;
+      $("#player-score-name").text(players[playerIndex]);
 
-  playerIndex++;
-  var playerName = document.getElementById('player-score-name');
-  playerName.innerText = teams[teamIndex][playerIndex];
+      $(".active").removeClass("active");
 
-  active[0].classList.remove('active');
-
-  if (playerIndex == 8) {
-    var scoringBox = document.getElementById('scoring-box');
-    var teamsSelectBox = document.getElementById('team-select-box');
-    var scoreComplete = document.getElementById('score-complete-box');
-
-    scoringBox.classList.add('hidden');
-    teamsSelectBox.classList.add('hidden');
-    scoreComplete.classList.remove('hidden');
-  }
+      if (players[playerIndex] == undefined) {
+        $("#scoring-box").addClass("hidden");
+        $("#team-select-box").addClass("hidden");
+        $("#score-complete-box").removeClass("hidden");
+      }
+    }
+  });
 }
 
-var acceptButton = document.getElementById('score-accept');
-acceptButton.addEventListener('click', advancePlayers);
+$("#score-accept").click(advancePlayers);
